de4bb1c021217a4c73238d95005d5a401e96aca5
eder64ghjhj <166666766+eder64ghjhj@users.noreply.github.com>
2024-06-12 21:15:16 -0300

    Add files via upload

--- banco (1).js
+++ banco (1).js
@@ -2,7 +2,7 @@ let nomeUsuario = prompt("Digite seu nome:");//Nome do usuario
 alert(`Olá ${nomeUsuario}, é um prazer ter você por aqui!`);
 
 let saldo = 1000; // saldo inicial fictício
-let senha = '1246'; // senha fictícia
+let senha = '3589'; // senha fictícia
 
 // função que cria opções para o cliente
 function inicio() {
